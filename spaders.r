ac4e150932b3f0e099f9b917393ceaa819a47244
David Ubilava <david.ubilava@sydney.edu.au>
2023-05-16 07:05:02 +1000

    updated logo dimensions

--- spaders.r
+++ spaders.r
@@ -20,17 +20,17 @@ d <- image_crop(d,"800x300+150+200")
 
 gg_spades <- ggplot(data.table(x=c(-1,1),y=c(-1,1)),aes(x=x,y=y))+
   geom_blank()+
-  geom_segment(aes(x=-.53,y=.69,xend=-.13,yend=.15),linewidth=3,color="slategray")+
-  geom_segment(aes(x=-.58,y=.48,xend=-.47,yend=.9),linewidth=3,color="slategray",lineend="round")+
-  geom_text(aes(x=0,y=0),label="SPA     ERS",size=16,fontface="bold",color="limegreen")+
-  geom_text(aes(x=0,y=-.85),label="Semi-Periodic Agricultural & Development Economics Research Symposium",size=2.2,family="Segoe Print",fontface="bold",color="slategray")+
+  geom_segment(aes(x=-.54,y=.62,xend=-.13,yend=.15),linewidth=3,color="slategray")+
+  geom_segment(aes(x=-.61,y=.39,xend=-.47,yend=.88),linewidth=3,color="slategray",lineend="round")+
+  geom_text(aes(x=0,y=0),label="SPA      ERS",size=16,fontface="bold",color="limegreen")+
+  geom_text(aes(x=0,y=-.85),label="Semi-Periodic Agricultural & Development Economics Research Symposium",size=2.3,family="Segoe Print",fontface="bold",color="slategray")+
   theme_void()+
   theme(panel.background=element_rect(fill="white",color=NA))
 
 gg_comb <- ggdraw(gg_spades) +
-  draw_image(d,scale=0.85,x=1.08,y=.22,hjust=1,halign=0,valign=0,clip="off")
+  draw_image(d,scale=0.85,x=1.08,y=.27,hjust=1,halign=0,valign=0,clip="off")
 
 gg_comb
 
-ggsave(gg_comb,file="logo.png",width=3.5,height=1.5,dpi=320)
+ggsave(gg_comb,file="logo.png",width=1280,height=640,dpi=320,units="px")
 
