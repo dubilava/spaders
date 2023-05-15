dff5d45e3e8fc228008d5e0cc7a449c1b63afcdc
David Ubilava <david.ubilava@sydney.edu.au>
2023-05-16 06:48:27 +1000

    some changes

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# spades
-Semi-Periodic Agricultural &amp; Development Economics Symposium
+# SPADERS
+
+This repository hosts material for the Semi-Periodic Agricultural &amp; Development Economics Research Symposium (SPADERS) held at the University of Sydney. 
+
+The inaugural meeting will happen on 19 May 2023. 
+
+As the name of the event suggests, this is envisaged to be a series of workshops held semi-periodically, often timed to maximize the presence of international visitors. 
+
+The goal of the event is to facilitate the dissemination of the existing research and, importantly, the emergence of new collaborative relationships among the participants. 
--- spaders.r
+++ spaders.r
@@ -20,15 +20,15 @@ d <- image_crop(d,"800x300+150+200")
 
 gg_spades <- ggplot(data.table(x=c(-1,1),y=c(-1,1)),aes(x=x,y=y))+
   geom_blank()+
-  geom_segment(aes(x=-.40,y=.68,xend=.0,yend=.14),linewidth=3,color="slategray")+
-  geom_segment(aes(x=-.46,y=.48,xend=-.35,yend=.9),linewidth=3,color="slategray",lineend="round")+
-  geom_text(aes(x=0,y=0),label="SPA     ES",size=16,fontface="bold",color="limegreen")+
-  geom_text(aes(x=0,y=-.85),label="Semi-Periodic Agricultural & Development Economics Symposium",size=2.2,family="Segoe Print",fontface="bold",color="slategray")+
+  geom_segment(aes(x=-.53,y=.69,xend=-.13,yend=.15),linewidth=3,color="slategray")+
+  geom_segment(aes(x=-.58,y=.48,xend=-.47,yend=.9),linewidth=3,color="slategray",lineend="round")+
+  geom_text(aes(x=0,y=0),label="SPA     ERS",size=16,fontface="bold",color="limegreen")+
+  geom_text(aes(x=0,y=-.85),label="Semi-Periodic Agricultural & Development Economics Research Symposium",size=2.2,family="Segoe Print",fontface="bold",color="slategray")+
   theme_void()+
   theme(panel.background=element_rect(fill="white",color=NA))
 
 gg_comb <- ggdraw(gg_spades) +
-  draw_image(d,scale=0.85,x=1.14,y=.22,hjust=1,halign=0,valign=0,clip="off")
+  draw_image(d,scale=0.85,x=1.08,y=.22,hjust=1,halign=0,valign=0,clip="off")
 
 gg_comb
 
